b15146a6ac1f3a54fee790d46e8862f364e9e22e
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-12-18 23:07:28 +0530

    reborn

---
+++ POS_ajax/src/filter/CORSFilter.java
@@ -0,0 +1,27 @@
+package filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * CopyWriteOwner - mr.Gunawardhana
+ * Contact - 071 - 733 1792
+ *
+ * © 2022 mGunawardhana,INC. ALL RIGHTS RESERVED.
+ */
+public class CORSFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
