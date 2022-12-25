5f364cbf4d028fdfc7a29da88881984355c3cb9d
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-12-25 10:30:35 +0530

    content update

--- POS_ajax/src/listener/DBConnection.java
+++ POS_ajax/src/listener/DBConnection.java
@@ -4,7 +4,7 @@
  * what's app - 071 - 9043372
  */
 
-package db;
+package listener;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
@@ -12,16 +12,17 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.sql.SQLException;
 
 @WebListener
 public class DBConnection implements ServletContextListener {
-
 
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         ServletContext servletContext = servletContextEvent.getServletContext();
         BasicDataSource basicDataSource = new BasicDataSource();
+
         basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         basicDataSource.setUrl("jdbc:mysql://localhost:3306/company");
         basicDataSource.setPassword("1234");
@@ -35,7 +36,11 @@ public class DBConnection implements ServletContextListener {
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-
+        try {
+            ((BasicDataSource) servletContextEvent.getServletContext().getAttribute("dbcp")).close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- POS_ajax/web/web/js/sample.js
+++
@@ -1,9 +0,0 @@
-function loadAllCustomers() {
-    $().empty();
-    $.ajax({
-        url: "http://localhost:8080/customer?option=GETALL"
-    ,method:"GET",
-        success : function (resp) {
-        }
-    })
-}
